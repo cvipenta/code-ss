2822b2348089c8231c0aaf288a83da3ea1f464d9
cvipenta <98951152+cvipenta@users.noreply.github.com>
2022-03-17 16:28:53 +0200

    use custom seeder using DB facade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // can be moved to UserSeeder::run method and called as
+        // $this->call(UserSeeder::class);
+        \DB::table('users')->insert([
+            'name' => 'Cristian V',
+            'email' => "ss@example.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
         User::factory(2)->create();
+        // end
 
         $this->call(MedicalTestCategorySeeder::class);
         $this->call(MedicalTestSeeder::class);
